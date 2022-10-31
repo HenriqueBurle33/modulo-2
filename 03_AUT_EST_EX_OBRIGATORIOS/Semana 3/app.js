cd44a2b7baabc4a1ba64067fb2cf5cf9b4aa8fbe
HenriqueBurle33 <110400696+HenriqueBurle33@users.noreply.github.com>
2022-10-31 14:56:09 -0300

    Add files via upload

---
+++ 03_AUT_EST_EX_OBRIGATORIOS/Semana 3/app.js
@@ -0,0 +1,40 @@
+const express = require('express'); 
+const app = express();
+
+const hostname = '127.0.0.1';
+const port = 3000;
+const sqlite3 = require('sqlite3').verbose();
+const DBPATH = 'banco de dados atividade.db'; //use o nome que você achar melhor para o banco de dados
+
+app.use(express.json());
+app.get('/curriculo', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    var db = new sqlite3.Database(DBPATH); // Abre o banco
+    var sql = 'SELECT * FROM curriculo';
+    db.all(sql, [],  (err, rows ) => {
+        if (err) {
+            throw err;
+        }
+        res.json(rows);
+    });
+    db.close(); // Fecha o banco
+});
+
+
+app.get('/formacao', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    var db = new sqlite3.Database(DBPATH); // Abre o banco
+    var sql = 'SELECT * FROM formacao';
+    db.all(sql, [],  (err, rows ) => {
+        if (err) {
+                throw err;
+         }
+        res.json(rows);
+    });
+    db.close(); // Fecha o banco
+});
+
+
+    
